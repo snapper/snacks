8b6c2049664799d85ada469058b54a57080f8ae4
Daniel Bogan <github.com@waferbaby.com>
2009-06-07 12:48:52 +1000

    Added the tweet and user id properties.

--- lib/models/tweet.rb
+++ lib/models/tweet.rb
@@ -3,6 +3,8 @@ class Tweet
     include DataMapper::Timestamp
     
     property :id, Serial
+    property :tweet_id, Integer
+    property :user_id, Integer
     property :contents, Text
 
     timestamps :at
