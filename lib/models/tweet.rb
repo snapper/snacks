2752fce4712f2d7eb8c64c7d92be7fab1a34075a
Daniel Bogan <github.com@waferbaby.com>
2009-08-03 17:32:29 +1000

    Updated to just subclass the Bite class.

--- lib/models/tweet.rb
+++ lib/models/tweet.rb
@@ -1,11 +1,2 @@
-class Tweet
-    include DataMapper::Resource
-    include DataMapper::Timestamp
-    
-    property :id, Serial
-    property :tweet_id, Integer
-    property :user_id, Integer
-    property :contents, Text
-
-    timestamps :at
+class Tweet < Bite
 end
