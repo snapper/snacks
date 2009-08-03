c50bbb5a61d1f5b8647f990fe9c0ed172b101c31
Daniel Bogan <github.com@waferbaby.com>
2009-08-03 17:34:45 +1000

    Reworked the configure method.

--- application.rb
+++ application.rb
@@ -1,29 +1,33 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'datamapper'
 require 'haml'
 require 'sass'
 
-configure do |app|
-    begin
-        config = YAML.load_file('config/application.yml')
-        
-        DataMapper.setup(:default, config[:database][app.environment])
-    	
-        Dir.glob('lib/models/*.rb') do |filename|
-            require filename
-	    end
-	    
-    rescue Exception => e
-        puts "Error: #{e.inspect}"
-        exit
+module Snacks
+    class App < Sinatra::Base
+       
+        configure do
+          root_path = Dir.pwd
+          
+          begin
+            config = YAML.load_file("#{root_path}/config/application.yml")
+          rescue Exception => e
+            puts "Snacks couldn't load its config file. #{e}"
+            exit
+          end
+          
+          environment = ENV['RACK_ENV'] || 'development'
+          
+          DataMapper.setup(:default, "sqlite3:///#{root_path}/databases/#{config[:database][environment.to_sym]}")
+          DataMapper::Logger.new(STDOUT, :debug)
+
+          %w[bite photo post tweet].each do |model|
+           require "#{root_path}/lib/#{model}.rb"
+          end
+        end
+     
+        get "/" do
+        end
     end
-end
-
-get "/" do
-end
-
-get "/tweets/?" do
-    @tweets = Tweet.all
-    haml :tweets
 end
