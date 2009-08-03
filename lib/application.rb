6ecdf604d528a0489d545e6ebcd2e94ff34916cc
Daniel Bogan <github.com@waferbaby.com>
2009-08-03 18:32:04 +1000

    Moved from root.

---
+++ lib/application.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'datamapper'
+require 'haml'
+require 'sass'
+
+module Snacks
+    class App < Sinatra::Base
+       
+        configure do
+          root_path = Dir.pwd
+          
+          begin
+            config = YAML.load_file("#{root_path}/config/application.yml")
+          rescue Exception => e
+            puts "Snacks couldn't load its config file. #{e}"
+            exit
+          end
+          
+          environment = ENV['RACK_ENV'] || 'development'
+          
+          DataMapper.setup(:default, "sqlite3:///#{root_path}/databases/#{config[:database][environment.to_sym]}")
+          DataMapper::Logger.new(STDOUT, :debug)
+
+          %w[bite photo tweet].each do |model|
+           require "#{root_path}/lib/#{model}.rb"
+          end
+        end
+     
+        get "/" do
+        end
+    end
+end
