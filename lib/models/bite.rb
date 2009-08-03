7761217ed72a0109742bb94303640779afebc060
Daniel Bogan <github.com@waferbaby.com>
2009-08-03 17:32:14 +1000

    Added url, type and creator properties.

--- lib/models/bite.rb
+++ lib/models/bite.rb
@@ -4,6 +4,9 @@ class Bite
     
     property :id, Serial
     property :title, String
+    property :url, String
+    property :type, Discriminator
+    property :creator, String
     property :contents, Text
 
     timestamps :at
